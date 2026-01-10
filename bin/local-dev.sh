53042fc188e398de7c4b4a04bc2c369572378171
Saul Baizman <saul@baizmandesign.com>
2026-01-10 14:30:24 -0500

    Added local-dev.sh.

---
+++ bin/local-dev.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env sh
+
+IMAGE_NAME="bzmn-wp-cli-package-image"
+CONTAINER_NAME="bzmn-wp-cli-package"
+DOCKERFILE="../Dockerfile"
+
+DOCKER="$(which docker)"
+
+# build the image and start the container
+echo "building Docker image and starting the ${IMAGE_NAME} container..."
+echo "${DOCKER} build -t ${IMAGE_NAME} . -f ${DOCKERFILE}"
+${DOCKER} build -t ${IMAGE_NAME} . -f ${DOCKERFILE}
+echo "${DOCKER} run --name \"${CONTAINER_NAME}\" -i -t ${IMAGE_NAME}"
+${DOCKER} run --name "${CONTAINER_NAME}" -i -t ${IMAGE_NAME}
+
+# to copy a file from the host to the container while it's running:
+# docker cp file.txt $(docker ps | grep bzmn-wp-cli-package-image | awk '{print$1}'):/root
